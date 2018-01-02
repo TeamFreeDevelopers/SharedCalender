e0f5d017e3ef2935ce4e6c3d5805edec8249c995
KUZUMOCHI\maso <ku2um0ch1894@gmail.com>
2018-01-02 17:43:35 +0900

    add new layout

--- app/src/main/java/teamfreedevelopers/sharedcalender/MainActivity.kt
+++ app/src/main/java/teamfreedevelopers/sharedcalender/MainActivity.kt
@@ -12,9 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // TODO Google APIを使うための認証をクリアする
+        // See:https://developers.google.com/google-apps/calendar/auth
+
         // TODO Google API叩いてデータ取得する
+        // See:https://developers.google.com/google-apps/calendar/v3/reference/calendarList/list
+//        val service = Calendar.Builder(httpTransport, jsonFactory, credentials)
+//                .setApplicationName("applicationName").build()
+//        CalendarList calendarList = service.calendarList().list().setPageToken(pageToken).execute()
 
         // TODO カレンダーのデータを詰める
+        // See:https://developers.google.com/google-apps/calendar/v3/reference/calendarList/list#examples
+//        while (calendarList.hasNext()) {
+//            //val items = calendarList.getItems()
+//
+//        }
 
         // TODO 選択した日の詳細情報をListに詰める
         val items = arrayOf("item a", "item b", "item c")
@@ -24,6 +36,5 @@ class MainActivity : AppCompatActivity() {
         listView.adapter = adapter
 
     }
-
 
 }
