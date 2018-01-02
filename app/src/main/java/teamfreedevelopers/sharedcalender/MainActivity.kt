493bc9c95275dd09dc78267b4acb922e03d26540
KUZUMOCHI\maso <ku2um0ch1894@gmail.com>
2018-01-02 17:15:52 +0900

    add new layout

--- app/src/main/java/teamfreedevelopers/sharedcalender/MainActivity.kt
+++ app/src/main/java/teamfreedevelopers/sharedcalender/MainActivity.kt
@@ -2,11 +2,22 @@ package teamfreedevelopers.sharedcalender
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val items = arrayOf("item a", "item b", "item c")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
+
+        listView.adapter = adapter
+
     }
+
+
 }
