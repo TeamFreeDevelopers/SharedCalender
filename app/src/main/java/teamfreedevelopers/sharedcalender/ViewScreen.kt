59975708a99bab54556af37e055ead313d852690
Emi Yaegashi <claisyu@gmail.com>
2018-01-02 15:16:11 +0900

    一旦コミット

---
+++ app/src/main/java/teamfreedevelopers/sharedcalender/ViewScreen.kt
@@ -0,0 +1,12 @@
+package teamfreedevelopers.sharedcalender
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewScreen {
+
+    public fun ViewCreate() {
+    //データ受取
+        var 
+    } 
+}
